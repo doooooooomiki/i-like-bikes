e6a8c0c1297e5d1f99fd5c446c3e021d78bb05da
Dominic <dominic.kirste@gmail.com>
2022-10-29 12:29:20 +0200

    fetched some data

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <h1>Home</h1>
+  <section>
+    <h1>
+      Home
+    </h1>
+    <ul>
+      <li v-for="product in data.data" :key="product.id">
+        <h2>
+          {{product.attributes.title}}
+        </h2>
+        <p>
+          {{product.attributes.description}}
+        </p>
+        <NuxtLink :to="`/products/${product.id}`">more</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup>
-  console.log('hi');
+  const { data } = await useFetch('http://localhost:1337/api/products/');
 </script>
