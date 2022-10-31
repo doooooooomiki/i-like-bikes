409b015c3ef76b4a678ab0dd0ca0fa4321c4dc17
Dominic <dominic.kirste@gmail.com>
2022-10-31 16:43:23 +0100

    using useFetch to fetch data by slug

---
+++ frontend/pages/bikes/[slug].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <pre>
+      {{$route}}
+      {{bike}}
+    </pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const route = useRoute()
+const { data: bike } = await useFetch(`http://localhost:1337/api/bikes?filters[slug]=${route.params.slug}`)
+
+</script>
