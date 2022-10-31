ccff5d937c7b6d96e81d19d4a756ca0f9985f662
Dominic <dominic.kirste@gmail.com>
2022-10-31 12:54:02 +0100

    added module @nuxtjs/strapi

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,13 @@
 <template>
   <section>
     <h1>i like bikes</h1>
+    <pre>
+      {{response}}
+    </pre>
   </section>
 </template>
 
-<script setup>
-
+<script setup lang="ts">
+const { find } = useStrapi()
+const response = await find<IBike>('bikes')
 </script>
