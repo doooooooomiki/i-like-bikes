d9e3aeb03b025daa76ee20afcf59b7f8a6bdbe9f
Dominic <dominic.kirste@gmail.com>
2022-11-01 00:47:50 +0100

    modified BikeTeaser

--- frontend/components/BikeTeaser.vue
+++ frontend/components/BikeTeaser.vue
@@ -1,8 +1,29 @@
 <template>
-  <NuxtLink :to="`/bikes/${bike.slug}`">{{bike.slug}}</NuxtLink>
+
+  <div class="bike-teaser">
+
+    <header>
+      <NuxtLink :to="`/photographers/${bike.photographer.data.attributes.slug}`">
+        {{bike.photographer.data.attributes.name}}
+      </NuxtLink>
+    </header>
+
+    <NuxtLink :to="`/bikes/${bike.slug}`">
+      <figure>
+        <figcaption>
+          {{bike.title}}
+        </figcaption>
+        <img :src="`${config.API_BASE_URL + bike.image.data.attributes.url}`" alt="">
+      </figure>
+    </NuxtLink>
+
+  </div>
+
 </template>
 
 <script lang="ts" setup>
+
+const config = useRuntimeConfig();
 
 // defineProps
 // https://vuejs.org/api/sfc-script-setup.html#defineprops-defineemits
@@ -14,3 +35,15 @@ defineProps<{
 }>()
 
 </script>
+
+<style scoped>
+
+.bike-teaser a {
+  display: inline-block;
+}
+
+.bike-teaser img {
+  width: 200px;
+}
+
+</style>
