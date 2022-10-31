6a86dc9dcc2671da4b4f75d3552a11ceca78532f
Dominic <dominic.kirste@gmail.com>
2022-10-31 16:42:25 +0100

    created BikeTeaser component

---
+++ frontend/components/BikeTeaser.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>
+      {{bike.name}}
+      <strong>{{bike.price}}</strong>
+    </p>
+    <p>
+      {{bike.description}}
+    </p>
+    <NuxtLink :to="`/bikes/${bike.slug}`">ride to bike</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  bike?: IBike
+}>()
+</script>
