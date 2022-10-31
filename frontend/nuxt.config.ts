9279f54f3d94bee2f05d56bc2a544941762eacbd
Dominic <dominic.kirste@gmail.com>
2022-10-31 17:09:14 +0100

    added env API_BASE_URL

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -8,4 +8,17 @@ export default defineNuxtConfig({
     cookieName: 'strapi_jwt'
   },
 
+  // runtimeConfig
+  // https://v3.nuxtjs.org/api/configuration/nuxt-config#runtimeconfig
+  // The value of this object is accessible from server only using useRuntimeConfig. 
+  // It mainly should hold private configuration which is not exposed on the frontend.
+  // Variables that need to be accessible on the server are added directly inside runtimeConfig. 
+  // Variables that need to be accessible on both the client and the server are defined in runtimeConfig.public.
+  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL || '/api'
+    }
+    
+  },
+
 })
--- frontend/pages/bikes/[slug].vue
+++ frontend/pages/bikes/[slug].vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <pre>
-      {{$route}}
       {{bike}}
     </pre>
   </div>
@@ -9,7 +8,22 @@
 
 <script setup lang="ts">
 
+// useRuntimeConfig
+// https://v3.nuxtjs.org/api/composables/use-runtime-config#useruntimeconfig
+// The useRuntimeConfig composable is used to expose config variables within your app.
+const config = useRuntimeConfig();
+
+// useRoute
+// https://v3.nuxtjs.org/api/composables/use-route#useroute
+// The useRoute composable returns the current route and must be called 
+// in a setup function, plugin, or route middleware.
 const route = useRoute()
-const { data: bike } = await useFetch(`http://localhost:1337/api/bikes?filters[slug]=${route.params.slug}`)
+
+const url = `${config.API_BASE_URL}/api/bikes?filters[slug]=${route.params.slug}`
+
+// useFetch
+// https://v3.nuxtjs.org/api/composables/use-fetch#usefetch
+// This composable provides a convenient wrapper around useAsyncData and $fetch.
+const { data: bike } = await useFetch(url)
 
 </script>
