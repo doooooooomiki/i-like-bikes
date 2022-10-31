55de7a0677de178662edddf95b9f0407c11e248c
Dominic <dominic.kirste@gmail.com>
2022-10-31 17:16:24 +0100

    added comment for defineProps

--- frontend/components/BikeTeaser.vue
+++ frontend/components/BikeTeaser.vue
@@ -12,7 +12,14 @@
 </template>
 
 <script lang="ts" setup>
+
+// defineProps
+// https://vuejs.org/api/sfc-script-setup.html#defineprops-defineemits
+// https://vuejs.org/api/sfc-script-setup.html#typescript-only-features
+// defineProps is a compiler macro only usable inside <script setup>. 
+// It does not need to be imported, and is compiled away when <script setup> is processed.
 const props = defineProps<{
   bike?: IBike
 }>()
+
 </script>
